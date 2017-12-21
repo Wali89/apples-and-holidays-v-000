bc3bafda5edad34f8beb61ef3a2a1fa639afc315
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-21 13:17:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_winter_holiday_supplies(holiday_hash)
       winter_supplies << supply
     end
   end
-  winter_supplies
+  winter_supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
